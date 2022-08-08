dcff352a2adf00ae1b588c6cb5363b60cb3bffa8
adgor <adgor000@gmail.com>
2022-08-08 14:25:47 +0200

    add depends

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,16 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import mdx from "@astrojs/mdx";
+import react from "@astrojs/react";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind()],
+  integrations: [tailwind(), mdx(), react(), image()],
+  vite: {
+    ssr: {
+      external: ["svgo"]
+    }
+  }
 });
