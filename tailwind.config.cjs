7af7639f51c0c46554911cdc28fa94d8508ca188
adgor <adgor000@gmail.com>
2022-08-12 12:36:18 +0200

    update bg @ nav && add heroHome

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,20 @@ module.exports = {
       colors: {
         primary: "var(--color-primary)",
         secondary: "var(--color-secondary)",
+        brand: {
+          richBlack: "#141E26",
+          gunmetal: "#233240",
+          charcoal: "#364959",
+          text: "#A3B8CC",
+          quickSilver: "#A6A6A6",
+          cultured: "#F2F2F2",
+          culturedDark: "#E0E0E0",
+          purple: "#5524BF",
+          lavenderFloral: "#B091F2",
+          blue: "#2405F2",
+          jonquil: "#F2CB05",
+          fireOpal: "#F26052",
+        },
       },
       textColor: {
         default: "var(--color-text)",
