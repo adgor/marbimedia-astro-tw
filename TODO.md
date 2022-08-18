e20d69f48b68ea9c94dec60e1b169ac467c06433
adgor <adgor000@gmail.com>
2022-08-18 09:31:40 +0200

    add products && update web section&comp

--- TODO.md
+++ TODO.md
@@ -1,11 +1,11 @@
 ### Todo
 
-- [ ] Responsive Sections: Products, Process, Contact  
-- [ ] prettier-plugin-tailwindcss [Link](https://github.com/tailwindlabs/prettier-plugin-tailwindcss)
+- [ ] prettier-plugin-tailwindcss [Link](https://github.com/tailwindlabs/prettier-plugin-tailwindcss)  
 
 ### In Progress
 
 
 ### Done ✓
 
+- [x] Responsive Sections: Products, Process, Contact  
 
---
+++ src/data/products.js
@@ -0,0 +1,38 @@
+export const devProcess = [
+  {
+    title: 'Gather Requirements',
+    iconName: 'mdi:abacus',
+    body: 'We put our ability to listen first and foremost. Our business analysts and professionals sit down with you to hear your needs.',
+  },
+  {
+    title: 'Write user stories',
+    iconName: 'mdi:abacus',
+    body: 'We use what we learned from the previous step to build user stories and use cases. We involve you in this process to ensure we stay on target.',
+  },
+  {
+    title: 'Estimate time & cost',
+    iconName: 'mdi:abacus',
+    body: 'This is where we pinpoint an accurate estimate of logistics involving your project— including but not limited to cost, timelines, and resources.',
+  },
+  
+  {
+    title: 'Add tasks to backlog',
+    iconName: 'mdi:abacus',
+    body:  "We fire up our project planners, define your project, assign tasks and get ready to sprint. We just can't wait to get started!",
+  },
+  {
+    title: 'Sprints for long run',
+    iconName: 'mdi:abacus',
+    body:  "This is the main step. We use our deeply-rooted agile methodologies to run a series of sprints to build your product, evaluating regularly.",
+  },
+  {
+    title: 'Iterate & refine',
+    iconName: 'mdi:abacus',
+    body:  "Nearing completion, we bring out our magnifying lenses and callipers to measure and inspect every single pixel, interaction, and edge-case.",
+  },
+  {
+    title: 'Result & delivery',
+    iconName: 'mdi:abacus',
+    body:  "Congratulations! You just got yourself an awesome product made by Monogram. This isn't the end of the journey— it's just the beginning.",
+  },
+]
