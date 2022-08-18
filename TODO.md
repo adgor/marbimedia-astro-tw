c1bf9fbb7c7884aae1c0a5ef1af27aba00a72402
adgor <adgor000@gmail.com>
2022-08-18 11:47:54 +0200

    add products && update design section&comp

--- TODO.md
+++ TODO.md
@@ -1,11 +1,13 @@
 ### Todo
 
+- [ ] add content
+- [ ] find images
+- [ ] find Font
+- [ ] Products comp add from products file fix css selector, change ml
 
 ### In Progress
 
-
 ### Done ✓
 
-- [x] prettier-plugin-tailwindcss [Link](https://github.com/tailwindlabs/prettier-plugin-tailwindcss)  
-- [x] Responsive Sections: Products, Process, Contact  
-
+- [x] prettier-plugin-tailwindcss [Link](https://github.com/tailwindlabs/prettier-plugin-tailwindcss)
+- [x] Responsive Sections: Products, Process, Contact
--- src/data/products.js
+++ src/data/products.js
@@ -1,38 +1,72 @@
 export const devProcess = [
   {
-    title: 'Gather Requirements',
-    iconName: 'mdi:abacus',
-    body: 'We put our ability to listen first and foremost. Our business analysts and professionals sit down with you to hear your needs.',
+    title: "Gather Requirements",
+    iconName: "mdi:abacus",
+    body: "We put our ability to listen first and foremost. Our business analysts and professionals sit down with you to hear your needs.",
   },
   {
-    title: 'Write user stories',
-    iconName: 'mdi:abacus',
-    body: 'We use what we learned from the previous step to build user stories and use cases. We involve you in this process to ensure we stay on target.',
+    title: "Write user stories",
+    iconName: "mdi:abacus",
+    body: "We use what we learned from the previous step to build user stories and use cases. We involve you in this process to ensure we stay on target.",
   },
   {
-    title: 'Estimate time & cost',
-    iconName: 'mdi:abacus',
-    body: 'This is where we pinpoint an accurate estimate of logistics involving your project— including but not limited to cost, timelines, and resources.',
+    title: "Estimate time & cost",
+    iconName: "mdi:abacus",
+    body: "This is where we pinpoint an accurate estimate of logistics involving your project— including but not limited to cost, timelines, and resources.",
   },
-  
+
   {
-    title: 'Add tasks to backlog',
-    iconName: 'mdi:abacus',
-    body:  "We fire up our project planners, define your project, assign tasks and get ready to sprint. We just can't wait to get started!",
-  },
-  {
-    title: 'Sprints for long run',
-    iconName: 'mdi:abacus',
-    body:  "This is the main step. We use our deeply-rooted agile methodologies to run a series of sprints to build your product, evaluating regularly.",
+    title: "Add tasks to backlog",
+    iconName: "mdi:abacus",
+    body: "We fire up our project planners, define your project, assign tasks and get ready to sprint. We just can't wait to get started!",
   },
   {
-    title: 'Iterate & refine',
-    iconName: 'mdi:abacus',
-    body:  "Nearing completion, we bring out our magnifying lenses and callipers to measure and inspect every single pixel, interaction, and edge-case.",
+    title: "Sprints for long run",
+    iconName: "mdi:abacus",
+    body: "This is the main step. We use our deeply-rooted agile methodologies to run a series of sprints to build your product, evaluating regularly.",
   },
   {
-    title: 'Result & delivery',
-    iconName: 'mdi:abacus',
-    body:  "Congratulations! You just got yourself an awesome product made by Monogram. This isn't the end of the journey— it's just the beginning.",
+    title: "Iterate & refine",
+    iconName: "mdi:abacus",
+    body: "Nearing completion, we bring out our magnifying lenses and callipers to measure and inspect every single pixel, interaction, and edge-case.",
   },
-]
+  {
+    title: "Result & delivery",
+    iconName: "mdi:abacus",
+    body: "Congratulations! You just got yourself an awesome product made by Monogram. This isn't the end of the journey— it's just the beginning.",
+  },
+];
+
+export const designProcess = [
+  {
+    title: "Paper first",
+    iconName: "mdi:abacus",
+    body: "Every design project we work on starts its journey as ink that flows onto a piece of paper. Our thoughts manifest through rapid ideation and we listen carefully.",
+  },
+  {
+    title: "Testing Prototypes",
+    iconName: "mdi:abacus",
+    body: "In the spirit of rapid ideation, as soon as we have a light bulb moment on paper, we take the process to build quick prototypes to demonstrate our thoughts.",
+  },
+  {
+    title: "Visual Design",
+    iconName: "mdi:abacus",
+    body: "This is when the design comes to life. We carefully craft each and every pixel of the design, down to the minor details, to create a real vision of the product.",
+  },
+
+  {
+    title: "Style Guide",
+    iconName: "mdi:abacus",
+    body: "Working with companies large and small, we understand the importance of making design repeatable and consistent. That's why we build detailed style guides.",
+  },
+  {
+    title: "Working UI Prototype",
+    iconName: "mdi:abacus",
+    body: "We take the UI design and create a moving, functional prototype that helps define just how your product would look and work. Usability is tested here.",
+  },
+  {
+    title: "We can build it",
+    iconName: "mdi:abacus",
+    body: "One of the biggest benefits of working with Monogram is we can build everything we design, and retain our top-notch quality of work to the development phase.",
+  },
+];
