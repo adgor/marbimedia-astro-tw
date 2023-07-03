5f71e2b12869e08b76380709fdb6958e136fb43e
adgor <adgor000@gmail.com>
2023-07-04 00:20:51 +0200

    add webContent

---
+++ src/data/webContent.ts
@@ -0,0 +1,211 @@
+export const home = [
+  {
+    hero: {
+      title1: "We bring Projects to Life",
+      title2: "Igniting Life into Projects",
+      subtitle1:
+        "MarbiMedia is a digital agency designing and developing the best websites, projects and experiences. LET'S WORK TOGETHER",
+      subtitle2:
+        "MarbiMedia stands as an innovative digital agency, dedicated to crafting exceptional websites, projects, and immersive experiences.",
+    },
+    ctaServices: {
+      title1: "Customers are looking for you online. We’ll help them find you.",
+      title2:
+        "Unleashing Your Online Presence: Guiding Customers to Discover You",
+      subtitle1:
+        "MarbiMedia is a digital agency built around passion and results. We help ambitious business of small and medium sizes to generate more revenue and growth.",
+      subtitle2:
+        "MarbiMedia: Where Passion Meets Results in the Digital Realm. Empowering Ambitious Small and Medium Businesses to Achieve Exponential Revenue and Growth.",
+    },
+    web: {
+      title1: "We build JAMstack experiences.",
+      title2:
+        "Unleashing the Power of JAMstack: Crafting Unforgettable Experiences",
+      title3:
+        "Elevate Your Digital Presence: Transforming Your Vision into JAMstack Experiences",
+      subtitle1:
+        "We use JAMstack architecture, which is the modern way to build Websites and Apps that delivers better performance, security, and scaling.",
+      subtitle2:
+        "Unlocking the Next Level of Web and App Development: Experience Unparalleled Performance, Security, and Scalability with our JAMstack Architecture",
+    },
+    design: {
+      title1: "Design is how it works",
+      title2:
+        "Crafting Experiences that Speak Volumes: Where Design Comes Alive in Functionality",
+      title3:
+        "Transforming Vision into Digital Masterpieces: Where Design Breathes Life into Functionality",
+      subtitle1:
+        "We provide the smart design, backed by the right technology, as the basis for the most impressive user experiences.",
+      subtitle2:
+        "Unlocking Extraordinary User Experiences: Fusing Intelligent Design and Cutting-Edge Technology for Unparalleled Impact",
+    },
+    project: {
+      title1: "Plan, build, and manage your project.",
+      title2: "Strategize, Construct, and Oversee Your Visionary Project",
+      title3:
+        "Pioneering Project Success: Blueprint, Construct, and Navigate Your Path to Achievement",
+      subtitle1:
+        "Empower Your Project's Journey: Expertly Orchestrating Planning, Construction, and Management for Seamless Success",
+      subtitle2:
+        "Empowering You to Effortlessly Navigate the Project Lifecycle, from Strategic Planning and Construction to Efficient Management and Success.",
+    },
+
+    ctaContact: {
+      title1: "Let’s bring your business noticable online",
+      title2:
+        "Unleash Your Business's Digital Visibility: Making Your Brand Unforgettable in the Online Realm",
+      subtitle1:
+        "We create advanced websites. Effective, attractive & simply unique.",
+      subtitle2:
+        "Igniting Digital Brilliance: Crafting Cutting-Edge Websites that Captivate, Convert, and Stand Out",
+    },
+    ctaContactForm: {
+      title1: "Don’t be stranger to talk to us?",
+      title2:
+        "Ignite the Conversation: Reach Out and Let's Create Something Extraordinary Together",
+      subtitle1:
+        "When you work with MarbiMedia, you’re joining a large, happy family that rides the wave of innovation to success. Drop us a line, and we’ll be in touch.",
+      subtitle2:
+        "Embark on an Exciting Journey: Join Our Thriving Innovation Family at MarbiMedia.Connect with Us Today, and Let's Ride the Wave to Success!",
+    },
+  },
+];
+
+export const webDev = [
+  {
+    hero: {
+      title1: "We bring Projects to Life",
+      title2: "Igniting Life into Projects",
+      subtitle1:
+        "MarbiMedia is a digital agency designing and developing the best websites, projects and experiences. LET'S WORK TOGETHER",
+      subtitle2:
+        "MarbiMedia stands as an innovative digital agency, dedicated to crafting exceptional websites, projects, and immersive experiences.",
+    },
+
+    website: {
+      title1: "Modern and Fast Websites & Apps",
+      title2: "Effective and attractive Websites & Apps",
+      title3:
+        "Revolutionizing the Digital Landscape: Sleek and Lightning-Fast Websites & Apps for the Modern Era",
+      subtitle1:
+        "We build modern and fast websites to meet the specific needs of your business that off-the-shelf software can't. Our technology choices making this possible is rooted in the belief that moves the web forward, making it secure, faster, and easier to scale.",
+      subtitle2:
+        "Unlock Your Business's Digital Potential: Custom-Built, Lightning-Fast Websites Tailored to Your Unique Needs. Embrace Our Forward-Thinking Technology to Elevate Security, Speed, and Scalability.",
+    },
+
+    appsData: {
+      title1: "Apps & Data",
+      title2: "",
+      subtitle1:
+        "We build applications designed to solve simple or complicated business needs. Our team has created dozens of solutions using all major platforms or creating development-intensive custom solutions.",
+      subtitle2:
+        "Elevating Efficiency and Insight: Transforming Your Business with Powerful Apps and Data-driven Solutions",
+    },
+
+    ecommerce: {
+      title1: "Ecommerce",
+      title2: "",
+      subtitle1:
+        "Ecommerce activity has seen an incredible rise over the past two years. We know that making the best online shopping experience is a combination of paying attention to detail and using cutting-edge technology.",
+      subtitle2:
+        "Ride the Ecommerce Wave: Crafting Unforgettable Online Shopping Experiences by Merging Precision and Cutting-Edge Technology",
+      subtitle3:
+        "Revolutionizing the Shopping Experience: Creating Seamless Ecommerce Solutions that Captivate Customers and Drive Success",
+    },
+  },
+];
+
+export const ourDevProcess = [
+  {
+    step1: {
+      title1: "Consultation",
+      subtitle1:
+        "It starts with a conversation where we'll determine what your project is about.",
+    },
+    step2: {
+      title1: "Proposal",
+      subtitle1:
+        "A detailed project proposal will be presented, followed by an action plan.",
+    },
+    step3: {
+      title1: "Creation",
+      subtitle1:
+        "Clients receive regular status updates as I work on and test the project.",
+    },
+    step4: {
+      title1: "Launch",
+      subtitle1:
+        "Your product comes to life with value added and peace of mind.",
+    },
+  },
+];
+
+export const Design = [
+  {
+    hero: {
+      title1: "We bring Projects to Life",
+      title2: "Igniting Life into Projects",
+      subtitle1:
+        "MarbiMedia is a digital agency designing and developing the best websites, projects and experiences. LET'S WORK TOGETHER",
+      subtitle2:
+        "MarbiMedia stands as an innovative digital agency, dedicated to crafting exceptional websites, projects, and immersive experiences.",
+    },
+
+    webApps: {
+      title1: "Web Applications",
+      title2: "",
+      subtitle1:
+        "Web apps and design systems including typography, grid systems, buttons, menus, sidebars, and more. We design the pieces to build your software more usable and beautiful than ever.",
+      subtitle2:
+        "Crafting Digital Marvels: Empowering Your Web Experience with Dynamic Design Systems, Encompassing Typography, Grids, Buttons, Menus, Sidebars, and Beyond. Unleash the Art of User-Friendly and Stunning Software, Assembled with Care.",
+    },
+
+    redesigns: {
+      title1: "Redesigns",
+      title2: "",
+      subtitle1:
+        "Probably one of the most satisfying things for us is to take a step back and see how we have improved software that was confusing, ugly, and unusable into something far better.",
+      subtitle2:
+        "Elevating the Extraordinary: Witness the Transformation: From Confusion, Ugliness, and Unusability to Astonishing Software Excellence",
+    },
+
+    websites: {
+      title1: "Spectacular Websites",
+      title2: "",
+      subtitle1:
+        "MarbiMedia has designed and developed hundreds of world-class web experiences with a quality that's on par or exceeding that of marketing and branding agencies. Our design just works.",
+      subtitle2:
+        "Elevating Web Experiences to Uncharted Heights: MarbiMedia's Unparalleled Design and Development Deliver World-Class Web Experiences, Surpassing Even Marketing and Branding Agencies. Our Designs Are Engineered for Flawless Functionality.",
+    },
+  },
+];
+
+export const ourDesignProcess = [
+  {
+    step1: {
+      title1: "Consultation",
+      subtitle1:
+        "It starts with a conversation where we'll determine what your project is about.",
+    },
+    step2: {
+      title1: "Proposal",
+      subtitle1:
+        "A detailed project proposal will be presented, followed by an action plan.",
+    },
+    step3: {
+      title1: "VISUAL DESIGN",
+      subtitle1:
+        "This is when the design comes to life. We carefully craft each and every pixel of the design, down to the minor details, to create a real vision of the product.",
+    },
+    step4: {
+      title1: "STYLE GUIDE",
+      subtitle1:
+        "Working with companies large and small, we understand the importance of making design repeatable and consistent. That's why we build detailed style guides.",
+    },
+    step5: {
+      title1: "WE CAN BUILD IT",
+      subtitle1:
+        "One of the biggest benefits of working with Monogram is we can build everything we design, and retain our top-notch quality of work to the development phase.",
+    },
+  },
+];
